797365169f228b1b7709c689090cfb23d5eded9e
rafrafciooo <drweedel@gmail.com>
2025-02-10 00:07:58 +0100

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 
-
 function App() {
-  return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-<Hero />
-<Features />
-
-
-    </main>
-  );
+	return (
+		<main className='relative min-h-screen w-screen overflow-x-hidden'>
+			<Hero />
+			<Features />
+			<section className='h-[300vh] bg-pink-700' />
+		</main>
+	);
 }
 
 export default App;
-
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
+	return <div className="">Features</div>;
+};
 
-export default Features
+export default Features;
